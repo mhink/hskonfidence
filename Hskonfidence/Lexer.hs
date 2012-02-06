0feaba16eb8e1657a21b342a7b7dd6e90ee1cd42
Matt Hink <mhink1103@gmail.com>
2012-02-06 10:22:50 -0600

    Adding Lexer functions

--- Hskonfidence/Lexer.hs
+++ Hskonfidence/Lexer.hs
@@ -4,3 +4,19 @@ module Hskonfidence.Lexer
   ) where
 
   import Hskonfidence.Token
+
+  lex :: String -> [Token]
+    --An empty string should return an array consisting only of one EOF token
+    lex [] = [ EOF ]
+
+    --A nonempty string should return a list of tokens
+    lex xs =
+      let ( token, remainingString ) = nextToken xs --parse next token
+      in  ( token : lex remainingString )           --construct token array
+
+  nextToken :: String -> ( Token, String )
+    --An empty string should return an EOF token and an empty remaining array
+    nextToken [] = ( EOF, [] )
+
+    --Strategy: check for complicated tokens first
+    --Strategy: then check for simple tokens
