91afe54f3cc155cae89a503f549ef4bb47efe6ab
Matt Hink <mhink1103@gmail.com>
2012-02-06 20:59:56 -0600

    Renamed Token value constructors, reworking Lexer

--- Hskonfidence/Lexer.hs
+++ Hskonfidence/Lexer.hs
@@ -1,22 +1,60 @@
 module Hskonfidence.Lexer
   (
-    lex    
+    lexer
   ) where
 
   import Hskonfidence.Token
 
-  lex :: String -> [Token]
-    --An empty string should return an array consisting only of one EOF token
-    lex [] = [ EOF ]
+  lexer :: String -> [Token]
+  lexer [] = [ EOF ]
 
-    --A nonempty string should return a list of tokens
-    lex xs =
-      let ( token, remainingString ) = nextToken xs --parse next token
-      in  ( token : lex remainingString )           --construct token array
+  lexer c:cs 
+    | isSpace c = lexer cs
+    | isAlpha c = lexVar c:cs
+    | isDigit c = lexNum c:cs
 
-  nextToken :: String -> ( Token, String )
-    --An empty string should return an EOF token and an empty remaining array
-    nextToken [] = ( EOF, [] )
+  lexer ("(":cs)      = TokenLPAREN   : lexer cs
+  lexer (")":cs)      = TokenRPAREN   : lexer cs
+  lexer ("[":cs)      = TokenLBRACK   : lexer cs
+  lexer ("]":cs)      = TokenRBRACK   : lexer cs
+  lexer ("{":cs)      = TokenLBRACE   : lexer cs
+  lexer (",":cs)      = TokenCOMMA    : lexer cs
+  lexer ("_":cs)      = TokenNEG      : lexer cs
+  lexer ("+":cs)      = TokenPLUS     : lexer cs
+  lexer ("-":cs)      = TokenMINUS    : lexer cs
+  lexer ("*":cs)      = TokenTIMES    : lexer cs
+  lexer ("/":cs)      = TokenDIV      : lexer cs
+  lexer ("//":cs)     = TokenFDIV     : lexer cs
+  lexer ("%":cs)      = TokenMOD      : lexer cs
+  lexer ("is":cs)     = TokenASSIGN   : lexer cs
+  lexer ("isis":cs)   = TokenEQ       : lexer cs
+  lexer ("isnt":cs)   = TokenNE       : lexer cs
+  lexer ("<=":cs)     = TokenLE       : lexer cs
+  lexer ("<":cs)      = TokenLT       : lexer cs
+  lexer (">=":cs)     = TokenGE       : lexer cs
+  lexer (">":cs)      = TokenGT       : lexer cs
+  lexer ("and":cs)    = TokenAND      : lexer cs
+  lexer ("or":cs)     = TokenOR       : lexer cs
+  lexer ("not":cs)    = TokenNOT      : lexer cs
+  lexer ("?":cs)      = TokenTERM : lexer cs
+  lexer ("array":cs)  = TokenARRAY : lexer cs
+  lexer ("char":cs)   = TokenCHAR : lexer cs
+  lexer ("float":cs)  = TokenFLOAT : lexer cs
+  lexer ("maybe":cs)  = TokenMAYBE : lexer cs
+  lexer ("int":cs)    = TokenINT : lexer cs
+  lexer ("read":cs) = TokenREAD : lexer cs
+  lexer ("whilst":cs) = TokenWHILST : lexer cs
+  lexer ("write":cs) = TokenWRITE : lexer cs
 
-    --Strategy: check for complicated tokens first
-    --Strategy: then check for simple tokens
+  --Convenience methods
+  isDoubleQuote :: Char -> Bool 
+  isDoubleQuote '"' = True
+  isDoubleQuote x = False
+
+  isSingleQuote :: Char -> Bool
+  isSingleQuote '\'' = True
+  isSingleQuote x = False
+
+  isDecimalPoint :: Char -> Bool
+  isDecimalPoint '.' = True
+  isDecimalPoint x = False
