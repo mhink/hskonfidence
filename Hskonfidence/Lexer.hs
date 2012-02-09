2a50af1a7bce2d26be1588a8950c9b19e8b5dda5
Matt Hink <mhink1103@gmail.com>
2012-02-08 21:33:14 -0600

    Lexing of integer, float, character, and string literals implemented.

--- Hskonfidence/Lexer.hs
+++ Hskonfidence/Lexer.hs
@@ -2,7 +2,9 @@ module Hskonfidence.Lexer
   (
     lexer,
     lexIdent,
-    lexIdent'
+    lexIdent',
+    lexNumer,
+    lexNumer'
   ) where
 
   import Hskonfidence.Token
@@ -45,33 +47,81 @@ module Hskonfidence.Lexer
   lexer ('?':cs)                         = TokenTERM      : lexer cs
   lexer [] = []
 
+  lexer ('\'':c:'\'':cs)                 = TokenCHRLIT c : lexer cs
+
   lexer (c:cs ) 
-    | isSpace c = lexer    (cs)
-    | isAlpha c = lexIdent (c:cs)
-    | isDigit c = lexNumer (c:cs)
+    | isSpace c       = lexer             (cs)
+    | isAlpha c       = lexIdent          (c:cs)
+    | isDigit c       = lexNumer          (c:cs)
+    | isDoubleQuote c = lexStringLiteral  (cs)
   lexer _ = [ TokenERROR ]
 
   lexIdent :: String -> [Token]
   lexIdent str = 
-      case lexIdent' (str) of
-        Nothing -> [TokenERROR]
-        Just result ->
-          let (resultStr, restOfStr) = result
-          in  TokenIDENT (resultStr) : lexer (restOfStr) 
+    case lexIdent' (str) of
+      Nothing -> [TokenERROR]
+      Just result ->
+        let (resultToken, restOfStr) = result
+        in  resultToken : lexer (restOfStr) 
 
-  lexIdent' :: String -> Maybe (String, String)
-  lexIdent' "" = Just ("", "")
+  lexIdent' :: String -> Maybe (Token, String)
+  lexIdent' "" = Just (TokenIDENT (""), "")
   lexIdent' (c:cs)
     | isAlphaNum c =             --continue recursion 
         case lexIdent' cs of
-          Just (id, rest) -> Just (c:id, rest)
+          Just (TokenIDENT (id), rest) -> Just (TokenIDENT(c:id), rest)
           Nothing         -> Nothing
-    | isSpace c = Just ([], cs)  --end recursion successfully
+    | isSpace c = Just (TokenIDENT "", cs)  --end recursion successfully
     | otherwise = Nothing        --end recursion unsuccessfully
 
   lexNumer :: String -> [Token]
-  lexNumer (c:cs) = TokenERROR : lexer cs
+  lexNumer str = 
+    let
+      (num, sep, rest) = lexNumer' str
+    in case sep of
+      ' ' -> TokenINTLIT (read num :: Integer) : lexer rest
+      '.' -> lexFloat (num, rest)
+      otherwise  -> [TokenERROR]
+      
+  lexNumer' :: String -> (String, Char, String)
+  lexNumer' [] = ("", ' ', "")
+  lexNumer' (c:cs)
+    | isDigit c = 
+      let (num, sep, rest) = lexNumer' cs
+      in  (c:num, sep, rest)
+    | otherwise = ("", c, cs)
 
+  lexFloat :: (String, String) -> [Token]
+  lexFloat (numToNow, rest) = 
+    case lexFloat' rest of
+      Nothing -> [TokenERROR]
+      Just result ->
+        let (fPartResult, restOfString) = result
+        in TokenFLOLIT (read (numToNow ++ "." ++ fPartResult) :: Float) : lexer restOfString
+
+  lexFloat' :: String -> Maybe(String, String)
+  lexFloat' "" = Just ("", "")
+  lexFloat' (c:cs)
+    | isDigit c =
+      case lexFloat' cs of
+        Just (num, rest) -> Just (c:num, rest)
+        Nothing          -> Nothing
+    | isSpace c = Just ("", cs)
+    | otherwise = Nothing
+
+  lexStringLiteral :: String -> [Token]
+  lexStringLiteral str =
+    case lexStringLiteral' str of
+      Nothing -> [TokenERROR]
+      Just (resultToken, rest) -> resultToken : lexer rest
+
+  lexStringLiteral' :: String -> Maybe (Token, String)
+  lexStringLiteral' [] = Nothing
+  lexStringLiteral' ('"':cs) = Just (TokenSTRLIT "", cs)
+  lexStringLiteral' (c:cs) =
+    case lexStringLiteral' cs of
+      Just (TokenSTRLIT (strLit), rest) -> Just (TokenSTRLIT(c:strLit), rest)
+      Nothing -> Nothing
 
   ----Convenience methods
   isDoubleQuote :: Char -> Bool 
